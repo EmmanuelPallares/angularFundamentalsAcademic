a1c1d446273c10a5149e97e78b5f156105636432
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-10-27 17:22:13 -0500

    class and style 2 way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
+
 import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  items: any[] = [
-    ['Platano', 10],
-    ['Manzana', 15],
-    ['Aguacate', 30],
-  ]; //Reto *ngFor
-  newItem = ''; //Reto *ngFor
-  newPrice = '';
+  widthImg = 10;
+  name = 'Nicolas';
+  age = 18;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  };
+  names: string[] = ['Nico', 'Juli', 'Santi'];
+  newName = '';
   products: Product[] = [
     {
       name: 'EL mejor juguete',
@@ -45,26 +52,7 @@ export class AppComponent {
       price: 3434,
       image: './assets/images/glasses.jpg',
     },
-  ]; //Reto *ngFor
-
-  addItem() {
-    this.items.push([this.newItem, this.newPrice]);
-    this.newItem = '';
-    this.newPrice = '';
-  }
-
-  deleteItem(index: number) {
-    this.items.splice(index, 1);
-  }
-  name = 'Nicolas';
-  age = 18;
-  img = 'https://www.w3schools.com/howto/img_avatar.png';
-  btnDisabled = true;
-  person = {
-    name: 'Nicolas',
-    age: 18,
-    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
-  };
+  ];
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
@@ -73,6 +61,7 @@ export class AppComponent {
   increaseAge() {
     this.person.age += 1;
   }
+
   onScroll(event: Event) {
     const element = event.target as HTMLElement;
     console.log(element.scrollTop);
@@ -81,5 +70,14 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
   }
 }
